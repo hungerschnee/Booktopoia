ae01dbe8e0f7ad3fc20f1553053fb7221defde7b
hungerschnee <dockhorn.benjamin@gmail.com>
2025-04-18 09:27:12 +0200

    refactoring

--- Frontend/pages/about.vue
+++ Frontend/pages/about.vue
@@ -12,7 +12,6 @@ const authorization = useState<UserStatus>('user-status');
   <div class="m-12 ">
     <h1 class="mb-4 text-3xl font-extrabold text-gray-500 dark:text-gray-200 md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-cyan-600">Booktopia</span> is a Student Project</h1>
     <p class="text-xl text-gray-500 dark:text-gray-200"> designed by Benjamin Dockhorn, Fabian Hils and Jannek Brödling </p>
-    <p class="text-xl text-gray-500 dark:text-gray-200">{{ authorization }}</p>
 
   </div>
 
